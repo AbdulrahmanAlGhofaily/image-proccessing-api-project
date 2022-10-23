fa891476b362df27e06f7588a8b58ffd4ad04b10
Abdulrahman <abdulrahman-alghofaily@outlook.com>
2022-10-23 10:37:31 +0300

    Build the project

--- app/build/routes/api/images.js
+++ app/build/routes/api/images.js
@@ -7,5 +7,4 @@ var express_1 = __importDefault(require("express"));
 var imageResizing_1 = __importDefault(require("../../middleware/imageResizing"));
 var images = express_1.default.Router();
 images.use(imageResizing_1.default);
-images.get('/', function (req, res) { });
 exports.default = images;
